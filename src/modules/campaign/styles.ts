91b974d9afcc46bdff857049b7641e13e3c5621f
Martin Bianchi <tinchola7@gmail.com>
2021-05-27 14:24:51 -0300

    feat: submit button variant. modify layout to fix button to the bottom

--- src/modules/campaign/styles.ts
+++ src/modules/campaign/styles.ts
@@ -1,8 +1,16 @@
 import { Style } from '../../types/Style'
 
 export const styles: Style = {
+  page: {
+    display: 'grid',
+    gridTemplateColumns: '1fr',
+    gridTemplateRows: '1fr max-content',
+    height: '100%'
+  },
   container: {
-    flexDirection: 'column'
+    flexDirection: 'column',
+    gridRow: '1/2',
+    overflow: 'auto'
   },
   campaignInfo: {
     flexDirection: 'column',
@@ -10,5 +18,9 @@ export const styles: Style = {
   },
   campaignTitle: {
     mb: 3
+  },
+  submitButton: {
+    gridRow: '2/3',
+    m: 1
   }
 }
