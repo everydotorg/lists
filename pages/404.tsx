0c5371add6a7a81b97db59097806d8c5632dc3d2
Martin Bianchi <tinchola7@gmail.com>
2021-07-27 10:19:19 -0300

    feat: add 404 error page (#212)
    * feat: add 404 error page
    
    * add padding to page

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+import { Error404 } from 'src/pages/Error404'
+
+const NotFoundPage = () => {
+  return <Error404 />
+}
+
+export default NotFoundPage
---
+++ src/pages/Error404/error404styles.ts
@@ -0,0 +1,17 @@
+import { Style } from 'types/Style'
+
+export const styles: Style = {
+  container: {
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    bg: 'backgroundGray',
+    p: [3, 6],
+    textAlign: 'center'
+  },
+  descriptionText: {
+    mb: [3, 6]
+  }
+}
