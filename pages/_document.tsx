6c65d9d3ba4685b2a5647c1a05741dd0925c09e9
Julian <poma.nob@gmail.com>
2021-08-13 20:42:37 -0300

    wip

---
+++ pages/_document.tsx
@@ -0,0 +1,57 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            src={'https://www.googletagmanager.com/gtag/js?id=GTM-PTXWSQK'}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              ;(function (w, d, s, l, i) {
+                w[l] = w[l] || []
+                w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
+                var f = d.getElementsByTagName(s)[0],
+                  j = d.createElement(s),
+                  dl = l != 'dataLayer' ? '&l=' + l : ''
+                j.async = true
+                j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
+                f.parentNode.insertBefore(j, f)
+              })(window, document, 'script', 'dataLayer', 'GTM-PTXWSQK')
+          `
+            }}
+          />
+        </Head>
+        <body>
+          <iframe
+            title="googletags"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PTXWSQK"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
