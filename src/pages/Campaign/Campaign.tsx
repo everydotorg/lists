e7f9a6dec4554d39044266ecc54355eb7bfe8c24
Martin Bianchi <tinchola7@gmail.com>
2021-08-17 17:48:58 -0300

    feat: add donation split message

--- src/pages/Campaign/Campaign.tsx
+++ src/pages/Campaign/Campaign.tsx
@@ -48,6 +48,12 @@ export const Campaign = (): JSX.Element => {
 
         {nonprofits && (
           <Box sx={styles.nonprofitsContainer}>
+            <Box sx={styles.nonprofitsTitleContainer}>
+              <Text variant="small" sx={styles.nonprofitsTitle}>
+                Your donation will be <strong>split evenly</strong> between
+                these 501(c)(3) charities
+              </Text>
+            </Box>
             <NonProfitList list={nonprofits} />
           </Box>
         )}
