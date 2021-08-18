e8eb682f0d30a4c306b846462551591cffaeed89
Martin Bianchi <tinchola7@gmail.com>
2021-08-18 08:40:06 -0300

    feat: keep monthly message (#312)

--- src/pages/Donation/Frequency/Frequency.tsx
+++ src/pages/Donation/Frequency/Frequency.tsx
@@ -3,10 +3,6 @@ import { DonationFrequency } from 'types/Frequency'
 import { styles } from './frequencyStyles'
 import React from 'react'
 import { Icon } from 'src/components/Icon'
-import { useTransition, config } from '@react-spring/core'
-import cubicBezier from 'bezier-easing'
-import { animated } from '@react-spring/web'
-
 interface FrequencyProps {
   frequency: DonationFrequency
   setFrequency: React.Dispatch<React.SetStateAction<DonationFrequency>>
@@ -16,32 +12,6 @@ export const Frequency = ({
   frequency,
   setFrequency
 }: FrequencyProps): JSX.Element => {
-  const transition = useTransition(frequency !== DonationFrequency.Monthly, {
-    from: {
-      transform: 'translateY(-50%)',
-      opacity: 0,
-      maxHeight: '0px'
-    },
-    enter: {
-      transform: 'translateY(0%)',
-      opacity: 1,
-      maxHeight: '400px'
-    },
-    leave: {
-      transform: 'translateY(-50%)',
-      opacity: 0,
-      maxHeight: '0px'
-    },
-    expires: true,
-    config: {
-      ...config.default,
-      duration: 250,
-      easing: cubicBezier(0.55, 0.08, 0, 1)
-    }
-  })
-
-  const AnimatedBox = animated(Box)
-
   return (
     <Box sx={styles.container}>
       <Label variant="text.small" sx={styles.label}>
@@ -91,22 +61,15 @@ export const Frequency = ({
           />
         </Label>
       </Flex>
-      {transition(
-        (style, show) =>
-          show && (
-            <AnimatedBox style={style}>
-              <Box sx={styles.donationContainer}>
-                <Box sx={styles.donationContainerArrow}>
-                  <Icon.ArrowUp />
-                </Box>
-                <Text as="p" variant="small" sx={styles.donationText}>
-                  Monthly gifts help nonprofits focus on their mission and
-                  long-term impact, not fundraising.
-                </Text>
-              </Box>
-            </AnimatedBox>
-          )
-      )}
+      <Box sx={styles.donationContainer}>
+        <Box sx={styles.donationContainerArrow}>
+          <Icon.ArrowUp />
+        </Box>
+        <Text as="p" variant="small" sx={styles.donationText}>
+          Monthly gifts help nonprofits focus on their mission and long-term
+          impact, not fundraising.
+        </Text>
+      </Box>
     </Box>
   )
 }
