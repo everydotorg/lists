184a6eb594c9effba6824fc4c1ff75a3ecdcf0da
Martin Bianchi <tinchola7@gmail.com>
2021-08-27 19:33:52 -0300

    try encoding uri

--- src/pages/ThankYou/ThankYou.tsx
+++ src/pages/ThankYou/ThankYou.tsx
@@ -36,7 +36,7 @@ export const ThankYou = (): JSX.Element | null => {
 
   const smsLink = () => {
     return isIOS
-      ? `sms://;?&body=${socialShareText}`
+      ? `sms://;?&body=${encodeURI(socialShareText)}`
       : `sms:?body=${socialShareText}`
   }
 
