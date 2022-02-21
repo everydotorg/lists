5fab27ce88a3cffbe59a8a9ced18425f5b8e0017
neucoas <poma.nob@gmail.com>
2022-02-21 14:20:48 -0300

    feat: add veteranssof campaign (#437)
    * feat: add veteranssof campaign
    
    * feat: add image and every slug

---
+++ src/campaigns/veteranssof.ts
@@ -0,0 +1,92 @@
+import { LocalCampaignInfo } from '../../types/CampaignInfo'
+
+export const veteranssof: LocalCampaignInfo = {
+  slug: 'veteranssof',
+  showOnHomepage: true,
+  everySlug: 'veteran-special-operation-forces',
+  name: 'Veteran Special Operation Forces',
+  primaryColor: '#00A37F',
+  mobileBannerUrl: 'v1644968017/specialops_j7uid7',
+  imageUrl: 'v1644968017/specialops_j7uid7',
+  bannerUrl: 'v1644968017/specialops_j7uid7',
+  socialShareText:
+    'Givelist to support Veteran Special Operation Forces and the Intelligence Community.',
+  previewImage: 'v1644968017/specialops_j7uid7',
+  about:
+    'Givelist to support Veteran Special Operation Forces and the Intelligence Community.',
+  cause: 'HUMAN_SERVICES',
+  nonprofits: [
+    {
+      slug: 'navy-seal-foundation',
+      name: 'Navy SEAL Foundation',
+      location: 'Virginia Beach, VA',
+      about:
+        'Provides immediate and ongoing support and assistance to the Naval Special Warfare community and its families.',
+      img: 'faja_profile/latt1xhvqxnpub5gby8w'
+    },
+    {
+      slug: 'greenberetfoundation',
+      name: 'Green Beret Foundation',
+      location: 'San Antonio, TX',
+      about:
+        'Our mission is to answer the call of the Green Berets and their families so that they can succeed in their next mission.',
+      img: 'faja_profile/gorepnjb3ahh4bwui1yz'
+    },
+    {
+      slug: 'pararescue-foundation',
+      name: 'Pararescue Foundation',
+      location: 'Milford, MI',
+      about:
+        'Our mission is to preserve and support our veterans from the USAF Pararescue community.',
+      img: 'profile_pics/tbpp81qg8tau8exgyyss'
+    },
+    {
+      slug: 'night-stalker-foundation',
+      name: 'Night Stalker Foundation',
+      location: 'Oak Grove, KY',
+      about:
+        'The Night Stalker Foundation provides emotional and financial support for Night Stalker families left behind.',
+      img: 'profile_pics/yxnyxdekdrjoxo38clgw'
+    },
+    {
+      slug: 'eod-warrior-foundation',
+      name: 'EOD Warrior Foundation',
+      location: 'Niceville, FL',
+      about:
+        'Improve the quality of life for the EOD Family by providing educational, financial and emotional support.',
+      img: '70882929_10157560925127288_8588717089030668288_o_oelq2t'
+    },
+    {
+      slug: 'specialoperators',
+      name: 'Special Operators Transition Foundation',
+      location: 'Valparaiso, FL',
+      about:
+        'We assist Special Operations Forces veterans with the successful transition to their next great career in corporate America.',
+      img: 'faja_profile/oahl3l8i91j0dilf6sxl'
+    },
+    {
+      slug: 'third-option-foundation',
+      name: 'Third Option Foundation',
+      location: 'Reston, VA',
+      about:
+        'Third Option Foundation seeks to heal, help, and honor members of the CIA’s Special Operations community and their families.',
+      img: 'profile_pics/yzym4dtg4ex2wwcq2pa1'
+    },
+    {
+      slug: 'ciamemorialfoundation',
+      name: 'CIA Officers Memorial Foundation',
+      location: 'Herndon, VA',
+      about:
+        'CIA Officers Memorial Foundation supports the well-being and educational needs of children and spouses of all fallen CIA officers.',
+      img: 'profile_pics/bza1jzvcjbafk9io1bk8'
+    },
+    {
+      slug: 'defense-intelligence-memorial-foundation',
+      name: 'Defense Intelligence Memorial Foundation',
+      location: 'Fairfax Sta, VA',
+      about:
+        'To honor the fallen officers of the Defense Intelligence Enterprise through scholarship and philanthropy.',
+      img: 'profile_pics/gazlxjpixdtioayiduvr'
+    }
+  ]
+}
